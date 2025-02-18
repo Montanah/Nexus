263b63d58d646d192d783467454c3f10242a03b1
Montanah <mititi.isaac@gmail.com>
2025-02-18 18:47:37 +0300

    Updated journeySteps.js to fix first step & landingPage.jsx to change thefont size for the title

--- frontendd/src/Constants/journeySteps.js
+++ frontendd/src/Constants/journeySteps.js
@@ -1,7 +1,7 @@
 export const JOURNEY_STEPS = [
     {
       title: "Create Order",
-      description: "Clients create product listings and specify delivery details",
+      description: "Clients create product listings and specifies delivery details",
       icon: "🛒"
     },
     {
