37b88b3e2131aa8a3e6b1058d940b9f96c221bc9
mumbuamutuku <mumbuamutuku@gmail.com>
2025-08-24 16:15:46 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -40,13 +40,12 @@ mongoose.connect(uri)
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Middleware
-// app.use(cors({
-//    // origin: true, 
-//     //credentials: true,
-//     //methods: ["GET", "POST", "PUT", "DELETE"], 
-//     //allowedHeaders: ['Authorization', 'Content-Type'],
-//   }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], 
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
